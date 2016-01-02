01226344a4b9f7133a1db14cd5195175a3c3c7f3
y2k <itwisterlx@gmail.com>
2016-01-02 17:16:09 +0400

    init commit

---
+++ android/src/main/kotlin/y2k/spectator/MainActivity.kt
@@ -0,0 +1,23 @@
+package y2k.spectator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private val counterStore = CounterStore()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my)
+
+        val counterTextView = findViewById(R.id.counterTextView) as TextView
+        val counterButton = findViewById(R.id.counterButton) as Button
+
+        counterButton.setOnClickListener { view ->
+            counterStore.add(1)
+            counterTextView.text = "Click Nr. " + counterStore.get()
+        }
+    }
+}
