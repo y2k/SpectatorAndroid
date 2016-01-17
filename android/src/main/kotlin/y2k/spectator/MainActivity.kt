304a11e8b94c5f02551352bacd773c4ceef2a1a7
y2k <itwisterlx@gmail.com>
2016-01-17 13:45:44 +0400

    android snapshot activity

--- android/src/main/kotlin/y2k/spectator/MainActivity.kt
+++ android/src/main/kotlin/y2k/spectator/MainActivity.kt
@@ -1,12 +1,21 @@
 package y2k.spectator
 
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        ActionBarDrawerToggle(this,
+            findViewById(R.id.drawer_layout) as DrawerLayout,
+            toolbar, R.string.app_name, R.string.app_name).syncState();
     }
 }
