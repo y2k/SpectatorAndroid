00429fa16b3aa5e0987ac7266bf398288bb9819f
y2k <itwisterlx@gmail.com>
2016-02-10 19:53:30 +0400

    ios версия

---
+++ ios/src/main/kotlin/y2k/spectator/MainViewController.kt
@@ -0,0 +1,22 @@
+package y2k.spectator
+
+import org.robovm.apple.uikit.UIButton
+import org.robovm.apple.uikit.UITableView
+import org.robovm.apple.uikit.UIViewController
+import org.robovm.objc.annotation.CustomClass
+import org.robovm.objc.annotation.IBOutlet
+
+/**
+ * Created by y2k on 10/02/16.
+ */
+@CustomClass("MainViewController")
+class MainViewController : UIViewController() {
+
+    @IBOutlet lateinit var list: UITableView
+    @IBOutlet lateinit var loginButton: UIButton
+
+    override fun viewDidLoad() {
+        super.viewDidLoad()
+        // TODO:
+    }
+}
